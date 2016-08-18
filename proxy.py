b38febae32405f7fec1ac5f36a4ef19510f34d18
Marco Cardacci <marcoc@ticketech.local>
2016-08-18 15:44:58 -0400

    adds proxy handler func

--- proxy.py
+++ proxy.py
@@ -21,6 +21,8 @@ def main():
 
     server_loop(local_host, local_port, remote_host, remote_port, receive_first)
 
+
+# The server_loop accepts incoming connections and spawns a new thread to the new connection
 def server_loop(local_host,local_port, remote_host, remote_port, receive_first):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -43,4 +45,95 @@ def server_loop(local_host,local_port, remote_host, remote_port, receive_first):
         # Start a thread to talk to the remote host
         proxy_thread = threading.Thread(target=proxy_handler, args=(client_socket, remote_host, remote_port, receive_first))
         proxy_thread.start()
+
+# 'proxy_handler' creates a TCP socket and connects to the remote host and port
+def proxy_handler(client_socket, remote_host, remote_port, receive_first):
+    remote_socket = socket.socket( socket.AF_INET, socket.SOCK_STREAM)
+    remote_socket.connect (( remote_host, remote_port ))
+    
+    # Here we check the 'receive_first' parameter
+    if receive_first:
+        remote_buffer = receive_from(remote_socket)
+        hexdump(remote_buffer)
+        remote_buffer = response_handler(remote_buffer)
+
+    # If we have data to send to the client, send it..
+    if len(remote_buffer):
+        print "[<==] Sending %d bytes to localhost." % len(remote_buffer)
+        client_socket.send(remote_buffer)
+
+    while 1:
+
+        # Reads from localhost if there's data
+        local_buffer = receive_from(client_socket)
+        if len(local_buffer):
+            print "[==>] Received %d bytes from localhost." % len(local_buffer)
+            # Processes data with hexdump function
+            hexdump(local_buffer)
+            # Sends data to localhost
+            local_buffer = request_handler(local_buffer)
+            remote_socket.send(local_buffer)
+            print "[==>] Sent to remote."
+        
+        # Reads from remote host if there's data
+        remote_buffer = receive_from(remote_socket)
+        if len(remote_buffer):
+            print "[==>] Received %d bytes from remote." % len(remote_buffer)
+            # Processes data with hexdump
+            hexdump(remote_buffer)
+            # Sends data to remote host
+            remote_buffer = response_handler(remote_buffer)
+            client_socket.send(remote_buffer)
+            print "[==>] Sent to localhost."
+
+        if not len(local_buffer) or not len(remote_buffer):
+            client_socket.close()
+            remote_socket.close()
+            print "[*] No more data. Closing connections"
+            break
+
+def receive_from(connection):
+#==========START HERE===============================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
